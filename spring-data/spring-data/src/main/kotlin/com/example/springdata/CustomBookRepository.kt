ac202b60ca52c29604fa8ffc77d1f40e03684ed0
Vincent Marillion <vmarilli@student.21-school.ru>
2021-11-15 13:48:01 +0300

    commit

--- spring-data/spring-data/src/main/kotlin/com/example/springdata/CustomBookRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.springdata
-
-import com.example.demo.entity.Book
-
-interface CustomBookRepository {
-    fun save(entity: Book)
-    fun findAll()
-    fun delete(entity: Book)
-}
--- spring-data/src/main/kotlin/com/example/springdata/SpringDataApplication.kt
+++ spring-data/src/main/kotlin/com/example/springdata/SpringDataApplication.kt
@@ -9,24 +9,6 @@ class SpringDataApplication (
     private val customBookRepository: CustomBookRepository
 ) : CommandLineRunner {
     override fun run(vararg args: String?) {
-//        val kotlinDev = Developer(firstName = "Вася", lastName = "Иванов", specialty = Specialty.KOTLIN, experience = 1)
-//        val kotlinDev2 = Developer(firstName = "Вася2", lastName = "Иванов", specialty = Specialty.KOTLIN, experience = 2)
-//        val javaDev1 = Developer(firstName = "Вася3", lastName = "Иванов", specialty = Specialty.JAVA, experience = 3)
-//        val javaDev2 = Developer(firstName = "Вася4", lastName = "Иванов", specialty = Specialty.JAVA, experience = 2)
-//
-//        developerRepository.saveAll(listOf(kotlinDev, kotlinDev2, javaDev1, javaDev2))
-
-//        val kotlinDevs = developerRepository.findBySpecialty(Specialty.KOTLIN)
-//        val skilledDevs = developerRepository.findSkilled(2)
-
-//        println(kotlinDevs)
-//
-//        println(skilledDevs)
-//        val pageResult = developerPagingRepository.findAll(PageRequest.of(1, 2))
-//        println("Total elements: ${pageResult.totalElements}")
-//        println("Total pages: ${pageResult.totalPages}")
-//        println("page 1: ${pageResult.content}")
-
         val resultAll = customBookRepository.findAll()
         println(resultAll)
     }
